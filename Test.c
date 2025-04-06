ac5b1231a43f28d643cc5276db31cfc8d776a585
hitu-1995 <110018373+hitu-1995@users.noreply.github.com>
2025-04-06 12:26:28 +0530

    Update Test.c

--- Test.c
+++ Test.c
@@ -8,10 +8,7 @@ void main()
     printf("Fourth Line Added by dev1\n");
     printf("Fifth Line Added by master\n");
     printf("Sixth Line Added by dev1\n");
-<<<<<<< HEAD
-    printf("Nineth Line Added by master\n");
-=======
     printf("Seventh Line Added by dev1\n");
     printf("Eight Line Added by dev1\n");
->>>>>>> b26e37faebec1431715ce4f257fad3d2be90d79b
+    printf("Nineth Line Added by master\n");
 }
